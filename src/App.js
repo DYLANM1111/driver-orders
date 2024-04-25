5cb24b545036f512bfa9fd82f23dde6200783b3f
csc340 <dmmuroki@uncg.edu>
2024-04-24 22:27:42 -0400

    going from 3 servers to one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Orders from "./pages/testOrders";
+import OrderDetails from "./pages/OrderDetails";
+import GPS from "./pages/GPS.jsx";
+import JoinRoom from "./pages/JoinRoom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/activeOrders" element={<Orders />} />
+          <Route path="/activeOrders/:id" element={<OrderDetails/>} />
+          <Route path="/gps" element={<GPS/>} />
+          <Route path="/chat" element={<JoinRoom/>} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
